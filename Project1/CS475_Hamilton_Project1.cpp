ab07391d5ad22d44fbb3a7c66718238ddec509de
bennetthamilton <bennetthamilton0@gmail.com>
2023-04-16 13:57:47 -0700

    added a loop in main to run varying thread counts and number of trials, uncommented lines so now it prints into a .cvs file

--- Project1/CS475_Hamilton_Project1.cpp
+++ Project1/CS475_Hamilton_Project1.cpp
@@ -11,19 +11,19 @@
 #endif
 
 // setting the number of threads:
-#ifndef NUMT
-#define NUMT		    2
-#endif
+// #ifndef NUMT
+// #define NUMT		    2
+// #endif
 
 // setting the number of trials in the monte carlo simulation:
-#ifndef NUMTRIALS
-#define NUMTRIALS	50000
-#endif
+// #ifndef NUMTRIALS
+// #define NUMTRIALS	50000
+// #endif
 
 // how many tries to discover the maximum performance:
 #define NUMTIMES	20
 
-//#define CSV
+#define CSV
 
 // the pins; numbers are constants:
 const float PinAx =	3.0f;
@@ -101,108 +101,121 @@ float Length( float dx, float dy )
 	return  sqrt( Sqr(dx) + Sqr(dy) );
 }
 
+void run( int numt, int numtrials)
+{
+    #ifdef _OPENMP
+        //fprintf( stderr, "OpenMP is supported -- version = %d\n", _OPENMP );
+    #else
+            fprintf( stderr, "No OpenMP support!\n" );
+            return;
+    #endif
 
+            TimeOfDaySeed( );               // seed the random number generator
+
+            omp_set_num_threads( numt );    // set the number of threads to use in parallelizing the for-loop:`
+
+            // better to define these here so that the rand() calls don't get into the thread timing:
+            float *holeaxs  = new float [numtrials];
+            float *holeays  = new float [numtrials];
+            float *holears  = new float [numtrials];
+
+            float *holebxs  = new float [numtrials];
+            float *holebys  = new float [numtrials];
+            float *holebrs  = new float [numtrials];
+
+            float *holecxs  = new float [numtrials];
+            float *holecys  = new float [numtrials];
+            float *holecrs  = new float [numtrials];
+
+            // fill the random-value arrays:
+            for( int n = 0; n < numtrials; n++ )
+            {
+                    holeaxs[n]  = Ranf(  HoleAx-HoleAxPM,  HoleAx+HoleAxPM );
+                    holeays[n]  = Ranf(  HoleAy-HoleAyPM,  HoleAy+HoleAyPM );
+                    holears[n]  = Ranf(  HoleAr-HoleArPM,  HoleAr+HoleArPM );
+
+                    holebxs[n]  = Ranf(  HoleBx-HoleBxPM,  HoleBx+HoleBxPM );
+                    holebys[n]  = Ranf(  HoleBy-HoleByPM,  HoleBy+HoleByPM );
+                    holebrs[n]  = Ranf(  HoleBr-HoleBrPM,  HoleBr+HoleBrPM );
+
+
+                    holecxs[n]  = Ranf(  HoleCx-HoleCxPM,  HoleCx+HoleCxPM );
+                    holecys[n]  = Ranf(  HoleCy-HoleCyPM,  HoleCy+HoleCyPM );
+                    holecrs[n]  = Ranf(  HoleCr-HoleCrPM,  HoleCr+HoleCrPM );
+            }
+
+            // get ready to record the maximum performance and the probability:
+            double  maxPerformance = 0.;    // must be declared outside the NUMTIMES loop
+            int     numSuccesses;           // must be declared outside the NUMTIMES loop
+
+
+            // looking for the maximum performance:
+            for( int times = 0; times < NUMTIMES; times++ )
+            {
+                    double time0 = omp_get_wtime( );
+
+                    numSuccesses = 0;
+
+            // note: the Pin numbers don't need to be declared shared( ) because they are const variables!
+                    #pragma omp parallel for default(none) shared(holeaxs,holeays,holears, holebxs,holebys,holebrs, holecxs,holecys,holecrs, stderr, PinAx, PinAy, PinAr, PinBx, PinBy, PinBr, PinCx, PinCy, PinCr, numtrials) reduction(+:numSuccesses)
+                    for( int n = 0; n < numtrials; n++ )
+                    {
+                        // randomize everything:
+                        float holeax = holeaxs[n];
+                        float holeay = holeays[n];
+                        float holear = holears[n];
+
+                        float holebx = holebxs[n];
+                        float holeby = holebys[n];
+                        float holebr = holebrs[n];
+
+                        float holecx = holecxs[n];
+                        float holecy = holecys[n];
+                        float holecr = holecrs[n];
+
+                        float da = Length( PinAx - holeax, PinAy - holeay );
+
+                        if( da + PinAr <= holear )
+                        {
+                            float db = Length( PinBx - holebx, PinBy - holeby );
+                            if( db + PinBr <= holebr )
+                            {
+                                float dc = Length( PinCx - holecx, PinCy - holecy );
+                                if( dc + PinCr <= holecr ) numSuccesses++;
+                            }
+                        }
+
+                    } // for( # of  monte carlo trials )
+
+                    double time1 = omp_get_wtime( );
+                    double megaTrialsPerSecond = (double)numtrials / ( time1 - time0 ) / 1000000.;
+                    if( megaTrialsPerSecond > maxPerformance )
+                            maxPerformance = megaTrialsPerSecond;
+
+            } // for ( # of timing tries )
+
+            float probability = (float)numSuccesses/(float)( numtrials );        // just get for last NUMTIMES run
+
+    #ifdef CSV
+            fprintf(stderr, "%2d, %8d, %6.2f, %6.2lf\n", numt, numtrials, 100.*probability, maxPerformance);
+    #else
+            fprintf(stderr, "%2d threads ; %8d trials ; probability = %6.2f ; megatrials/sec = %6.2lf\n",
+                    numt, numtrials, 100.*probability, maxPerformance);
+    #endif
+}
 
 int main( int argc, char *argv[ ] )
 {
-#ifdef _OPENMP
-	//fprintf( stderr, "OpenMP is supported -- version = %d\n", _OPENMP );
-#else
-        fprintf( stderr, "No OpenMP support!\n" );
-        return 1;
-#endif
 
-        TimeOfDaySeed( );               // seed the random number generator
+    int numThreads[] = {1, 2, 4, 8, 12, 16, 20, 24, 32};
+    int numTrials[] = {1, 10, 100, 1000, 10000, 100000, 500000, 1000000};
 
-        omp_set_num_threads( NUMT );    // set the number of threads to use in parallelizing the for-loop:`
-
-        // better to define these here so that the rand() calls don't get into the thread timing:
-        float *holeaxs  = new float [NUMTRIALS];
-        float *holeays  = new float [NUMTRIALS];
-        float *holears  = new float [NUMTRIALS];
-
-        float *holebxs  = new float [NUMTRIALS];
-        float *holebys  = new float [NUMTRIALS];
-        float *holebrs  = new float [NUMTRIALS];
-
-        float *holecxs  = new float [NUMTRIALS];
-        float *holecys  = new float [NUMTRIALS];
-        float *holecrs  = new float [NUMTRIALS];
-
-        // fill the random-value arrays:
-        for( int n = 0; n < NUMTRIALS; n++ )
+    for( int thread : numThreads )
+    {
+        for( int trials : numTrials)
         {
-                holeaxs[n]  = Ranf(  HoleAx-HoleAxPM,  HoleAx+HoleAxPM );
-                holeays[n]  = Ranf(  HoleAy-HoleAyPM,  HoleAy+HoleAyPM );
-                holears[n]  = Ranf(  HoleAr-HoleArPM,  HoleAr+HoleArPM );
-
-                holebxs[n]  = Ranf(  HoleBx-HoleBxPM,  HoleBx+HoleBxPM );
-                holebys[n]  = Ranf(  HoleBy-HoleByPM,  HoleBy+HoleByPM );
-                holebrs[n]  = Ranf(  HoleBr-HoleBrPM,  HoleBr+HoleBrPM );
-
-
-                holecxs[n]  = Ranf(  HoleCx-HoleCxPM,  HoleCx+HoleCxPM );
-                holecys[n]  = Ranf(  HoleCy-HoleCyPM,  HoleCy+HoleCyPM );
-                holecrs[n]  = Ranf(  HoleCr-HoleCrPM,  HoleCr+HoleCrPM );
+            run(thread, trials);
         }
-
-        // get ready to record the maximum performance and the probability:
-        double  maxPerformance = 0.;    // must be declared outside the NUMTIMES loop
-        int     numSuccesses;           // must be declared outside the NUMTIMES loop
-
-
-        // looking for the maximum performance:
-        for( int times = 0; times < NUMTIMES; times++ )
-        {
-                double time0 = omp_get_wtime( );
-
-                numSuccesses = 0;
-
-		// note: the Pin numbers don't need to be declared shared( ) because they are const variables!
-                #pragma omp parallel for default(none) shared(holeaxs,holeays,holears, holebxs,holebys,holebrs, holecxs,holecys,holecrs, stderr, PinAx, PinAy, PinAr, PinBx, PinBy, PinBr, PinCx, PinCy, PinCr) reduction(+:numSuccesses)
-                for( int n = 0; n < NUMTRIALS; n++ )
-                {
-                    // randomize everything:
-                    float holeax = holeaxs[n];
-                    float holeay = holeays[n];
-                    float holear = holears[n];
-
-                    float holebx = holebxs[n];
-                    float holeby = holebys[n];
-                    float holebr = holebrs[n];
-
-                    float holecx = holecxs[n];
-                    float holecy = holecys[n];
-                    float holecr = holecrs[n];
-
-                    float da = Length( PinAx - holeax, PinAy - holeay );
-
-                    if( da + PinAr <= holear )
-                    {
-                        float db = Length( PinBx - holebx, PinBy - holeby );
-                        if( db + PinBr <= holebr )
-                        {
-                            float dc = Length( PinCx - holecx, PinCy - holecy );
-                            if( dc + PinCr <= holecr ) numSuccesses++;
-                        }
-                    }
-
-                } // for( # of  monte carlo trials )
-
-                double time1 = omp_get_wtime( );
-                double megaTrialsPerSecond = (double)NUMTRIALS / ( time1 - time0 ) / 1000000.;
-                if( megaTrialsPerSecond > maxPerformance )
-                        maxPerformance = megaTrialsPerSecond;
-
-        } // for ( # of timing tries )
-
-        float probability = (float)numSuccesses/(float)( NUMTRIALS );        // just get for last NUMTIMES run
-
-#ifdef CSV
-        fprintf(stderr, ????? );
-#else
-        fprintf(stderr, "%2d threads : %8d trials ; probability = %6.2f ; megatrials/sec = %6.2lf\n",
-                NUMT, NUMTRIALS, 100.*probability, maxPerformance);
-#endif
+    } 
 
 }
